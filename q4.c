6441a802ab08fd30561b75dad9925c70847330cc
princegarg618-cpu <soulgarg618@gmail.com>
2025-08-27 12:50:15 +0530

    Add files via upload

---
+++ q4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int radius=7;
+    float P;
+    float A;
+    P=2*3.14*radius;
+    A=3.14*radius*radius;
+    printf("Area is %f \n",A);
+    printf("Perimeter is %f \n",P);
+    return 0;
+}
