37a69f4510864de4cbc8680112605bc4b53f2b63
princegarg618-cpu <soulgarg618@gmail.com>
2025-08-27 12:37:34 +0530

    done

---
+++ q2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+    int a, b;
+    printf("enter number a and b \n");
+    scanf("%d %d",&a,&b);
+    printf("sum is %d\n",(a+b));
+    printf("difference is %d\n",(a-b));
+     printf("product is %d\n",(a*b));
+      printf("division is %d\n",(a/b));
+    return 0;
+}
+        
