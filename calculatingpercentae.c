59b1e0bdd89ef9eaf84012099938c8dca6478694
princegarg618-cpu <soulgarg618@gmail.com>
2025-08-27 12:38:19 +0530

    done

---
+++ calculatingpercentae.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    float m1=95; // maths marks
+    float M=95; // physics marks
+    float m3=96; // chemistry marks
+    float m4=98; // pe marks
+    float m5=99; // english marks
+    float p= (m1 + M + m3 + m4 + m5)/5;
+    printf("Percentage of 5 subjects is ; %f",p);
+
+    return 0;
+}
