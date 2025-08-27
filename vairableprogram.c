f2d4b6cc52afcf8763c4ec29e09d5ad99f31a70f
princegarg618-cpu <soulgarg618@gmail.com>
2025-08-27 12:39:11 +0530

    done

---
+++ vairableprogram.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+    int x;
+    x = 5;
+    printf("\n%d",x);
+    x = x+5;
+    printf("\n%d",x);
+    x = 100;
+    x = x-20;
+    printf("\n%d",x);
+    return 0;
+
+}
+ 
