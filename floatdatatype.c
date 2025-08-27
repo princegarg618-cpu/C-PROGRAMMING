b8ad7e2762807bf1225b3dc29871854e14822cd6
princegarg618-cpu <soulgarg618@gmail.com>
2025-08-27 12:38:43 +0530

    done

---
+++ floatdatatype.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    printf("hello world");
+    float x=3.14;
+    printf("\n%f",x);
+    
+    float y = 20.178;
+    printf("\n%f",y);
+
+    float z=x+y;
+    printf("\n%f",z);
+
+    float m=5.28;
+    printf("\n%f",m);
+    float f=3.20;
+    printf("\n%f",f);
+    
+    return 0;
+}
